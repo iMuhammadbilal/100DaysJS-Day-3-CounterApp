d9c74f77b4a275ce0e5754c4140f383e8e97e106
bilal-marwat <bilalmarwat134@gmail.com>
2025-02-02 08:51:42 +0500

    Add some more interactivity

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 // first targer three buttons
-
 const button1 = document.getElementById('increase');
 const button2 = document.getElementById('decrease');
 const button3 = document.getElementById('reset');
-
 // show message in page
 const message = document.getElementById('count');
-
-
 // add event listener to first button
 button1.addEventListener('click', () => {
-
     // message will be show in page when user clicks the button
     count.innerHTML = parseInt(count.innerHTML) + 1;
-
     // change color of count based on the value
     if(count.innerHTML < 10){
         count.style.color = 'green';
@@ -29,12 +23,10 @@ button1.addEventListener('click', () => {
         count.style.color = 'black';
     }
 });
-
 // add event listener to second button
 button2.addEventListener('click', () => {
     // message will be show in page when user clicks the button
     count.innerHTML = parseInt(count.innerHTML) - 1;
-
     // change color of count based on the decrease value
     if(count.innerHTML < 10){
         count.style.color = '#e56b6f';
@@ -48,13 +40,10 @@ button2.addEventListener('click', () => {
         count.style.color = '#e26d5c';
     }else{
         count.style.color = 'black';
-    }
-    
+    } 
 });
-
 // add event listener to third button
 button3.addEventListener('click', () => {
     // message will be show in page when user clicks the button
     count.innerHTML = 0;
-
 });
